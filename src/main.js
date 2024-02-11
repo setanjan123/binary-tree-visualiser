f1136cf7e46af6bb640c1b60607a9b6eef7e4957
Setanjan Roy <setanjan123@gmail.com>
2024-02-11 15:49:00 +0530

    Handled orphan nodes

--- src/main.js
+++ src/main.js
@@ -8,11 +8,13 @@ class Node {
     }
 }
 
+let orphans = [];
+
 const buildTreeObject = (node, i,length,arr) => {
     if (i < length) {
         node = new Node(arr[i]);
         let left = buildTreeObject(node.left, 2 * i + 1,length,arr);
-        let right = buildTreeObject(node.right, 2 * i + 2,length,arr);    
+        let right = buildTreeObject(node.right, 2 * i + 2,length,arr);  
         if(left && right && left.name!=='null' && right.name!=='null') {
             node.children.push(left);
             node.children.push(right);
@@ -24,6 +26,10 @@ const buildTreeObject = (node, i,length,arr) => {
         else if(right && right.name!=='null') {
             node.children.push(new Node('null'));
             node.children.push(right);
+        }
+        if(node.name=='null' && node.children.length>0) {
+             if(node.children[0]?.name!='null') orphans.push(node.children[0].name)
+             if(node.children[1]?.name!='null') orphans.push(node.children[1].name)
         }
     }
     return node;
@@ -37,6 +43,7 @@ function buildTree(arrString) {
 }
 
 const drawTree = ()=> {
+    document.getElementById('message').innerHTML = "";
     const treeData = buildTree(document.getElementById('array').value); 
     const margin = { top: 40, right: 90, bottom: 50, left: 90 };
     const width = 600 - margin.left - margin.right;
@@ -87,6 +94,11 @@ const drawTree = ()=> {
     .attr("y", 0)
     .style("text-anchor", "middle")
     .text(d => d.data.name);
+
+    if(orphans.length>0) {
+         document.getElementById('message').innerHTML = 'Orphan Nodes were not drawn :'+orphans;
+         orphans = [];
+    }
 }
 
 window.drawTree = drawTree;
